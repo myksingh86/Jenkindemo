88c6d0be2ce79a03335d957322828a3a3e6a1521
mayank singh <mayanksingh.gahlot@singtel.com>
2017-11-01 17:38:53 +0800

    test

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "World1";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
